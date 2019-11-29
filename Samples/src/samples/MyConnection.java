4f022fcd44563a6f001d099c37df2ce51076d293
sunitne_sd2082@CEACCS.NET <sunitne_sd2082@CEACCS.NET>
2019-11-29 17:09:47 +0800

    Trial_Pharmacy

---
+++ Samples/src/samples/MyConnection.java
@@ -0,0 +1,28 @@
+
+package samples;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author sunitne_sd2082
+ */
+public class MyConnection {
+    
+    public static Connection getConnection(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/Register_Login","root","");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+}
